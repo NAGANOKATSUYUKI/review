b60dc33ff96f6a6d37bac379ede2ea1fc4869d57
NAGANOKATSUYUKI <n.katuyuki2001@gmail.com>
2022-10-27 15:57:18 +0900

    変更

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,7 +12,7 @@
     <body class="antialiased">
         <h1>Review Name</h1>
         <div class="posts">
-            @foreach( $posts as $post)
+            @foreach( $posts as $hensu2)
                 <div class="post">
                     <h2 class="title">{{ $post->title }}</h2>
                     <p class="body">{{ $post->body }}</p>
